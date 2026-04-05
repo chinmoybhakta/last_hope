55fc92b0ea9d8cd247c67d837da4fe5d8a881723
Protiva Bose <protivabose94@gmail.com>
2026-04-05 13:02:11 +0600

    feat(splash screen): add splash screen and all assets

---
+++ lib/feature/splash_screen/splash_screen.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+import '../onboarding/onboarding_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const OnboardingScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox.expand(
+        child: Image.asset(
+          'assets/images/splash.png',
+          fit: BoxFit.fill,
+        ),
+      ),
+    );
+  }
+}
