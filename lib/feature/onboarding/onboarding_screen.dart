2b2d8c1730aa2c3c5674695b556391334425a0d3
Protiva Bose <protivabose94@gmail.com>
2026-04-05 12:58:39 +0600

    feat(onboarding screen): add onboarding screen

---
+++ lib/feature/onboarding/onboarding_screen.dart
@@ -0,0 +1,354 @@
+import 'dart:math';
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+import '../home_screens/nasa_home.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  final PageController _controller = PageController();
+
+  final List<List<String>> images = [
+    [
+      "assets/onboarding/on1.png",
+      "assets/onboarding/on2.png",
+      "assets/onboarding/on3.png",
+    ],
+    [
+      "assets/onboarding/on4.png",
+      "assets/onboarding/on5.png",
+      "assets/onboarding/on6.png",
+    ],
+    [
+      "assets/onboarding/on7.png",
+      "assets/onboarding/on8.png",
+      "assets/onboarding/on9.png",
+    ],
+  ];
+
+  final List<String> texts = [
+    "IN SILENCE,\nIN DISTANCE,\nIN UNCERTAINTY-",
+    "Maps to guide you\nKnowledge to protect you\nIntelligence to help\nyou survive.",
+    "LAST HOPE STANDS\nREADY.",
+  ];
+
+  void goToHome() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (_) => const LastHopeScreen()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.blueAccent.shade700,
+              const Color(0xFFEF6C00),
+              const Color(0xFF1C1C1C),
+            ],
+            stops: const [0.2, 0.6, 1.0],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: PageView.builder(
+          controller: _controller,
+          itemCount: 3,
+          itemBuilder: (context, index) {
+            return OnboardPage(
+              images: images[index],
+              text: texts[index],
+              onSkip: goToHome,
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class OnboardPage extends StatefulWidget {
+  final List<String> images;
+  final String text;
+  final VoidCallback onSkip;
+
+  const OnboardPage({
+    super.key,
+    required this.images,
+    required this.text,
+    required this.onSkip,
+  });
+
+  @override
+  State<OnboardPage> createState() => _OnboardPageState();
+}
+
+class _OnboardPageState extends State<OnboardPage>
+    with TickerProviderStateMixin {
+  late AnimationController _animController;
+  late AnimationController _rotateController;
+  late Animation<double> _scaleAnim;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 800),
+    );
+
+    _scaleAnim = Tween(begin: 0.7, end: 1.0).animate(
+      CurvedAnimation(parent: _animController, curve: Curves.easeOutBack),
+    );
+
+    _animController.forward();
+
+    _rotateController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 6),
+    )..repeat();
+  }
+
+  @override
+  void didUpdateWidget(covariant OnboardPage oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _animController.reset();
+    _animController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animController.dispose();
+    _rotateController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Stack(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimatedGradientText(),
+              SizedBox(height: 40,),
+              CarouselSlider(
+                options: CarouselOptions(
+                  height: 200,
+                  autoPlay: true,
+                  enlargeCenterPage: true,
+                  viewportFraction: 0.8,
+                ),
+                items: widget.images.map((img) {
+                  return ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.asset(
+                      img,
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                    ),
+                  );
+                }).toList(),
+              ),
+
+              const SizedBox(height: 40),
+
+              SizedBox(
+                width: 250,
+                height: 220,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    AnimatedBuilder(
+                      animation: _rotateController,
+                      builder: (context, child) {
+                        return Transform.rotate(
+                          angle: _rotateController.value * 2 * pi,
+                          child: CustomPaint(
+                            size: const Size(220, 220),
+                            painter: CircleLinesPainter(),
+                          ),
+                        );
+                      },
+                    ),
+
+
+                    ScaleTransition(
+                      scale: _scaleAnim,
+                      child: Text(
+                        widget.text,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 13,
+                          letterSpacing: 1.2,
+                          fontWeight: FontWeight.w500,
+                          shadows: [
+                            Shadow(
+                              blurRadius: 10,
+                              color: Colors.black54,
+                              offset: Offset(2, 2),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+
+      Positioned(
+      top: 10,
+      right: 15,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 6),
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.15),
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(
+                color: Colors.white.withOpacity(0.3),
+                width: 1,
+              ),
+            ),
+            child: TextButton(
+              onPressed: widget.onSkip,
+              style: TextButton.styleFrom(
+                padding: EdgeInsets.zero,
+                minimumSize: Size.zero,
+                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              ),
+              child: const Text(
+                "Skip",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+        ],
+      ),
+    );
+  }
+}
+
+class CircleLinesPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.white
+      ..strokeWidth = 2;
+
+    final center = Offset(size.width / 2, size.height / 2);
+    final radius = size.width / 2;
+
+    const int lines = 12;
+
+    for (int i = 0; i < lines; i++) {
+      double angle = (2 * pi / lines) * i;
+
+      final start = Offset(
+        center.dx + (radius - 20) * cos(angle),
+        center.dy + (radius - 20) * sin(angle),
+      );
+
+      final end = Offset(
+        center.dx + radius * cos(angle),
+        center.dy + radius * sin(angle),
+      );
+
+      canvas.drawLine(start, end, paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+}
+class AnimatedGradientText extends StatefulWidget {
+  const AnimatedGradientText({super.key});
+
+  @override
+  State<AnimatedGradientText> createState() => _AnimatedGradientTextState();
+}
+
+class _AnimatedGradientTextState extends State<AnimatedGradientText>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return ShaderMask(
+          shaderCallback: (bounds) {
+            return LinearGradient(
+              colors:  [
+                Colors.blue.shade900,
+                Colors.orange,
+                Colors.blue.shade900,
+              ],
+              stops: [
+                (_controller.value - 0.3).clamp(0.0, 1.0),
+                _controller.value,
+                (_controller.value + 0.3).clamp(0.0, 1.0),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ).createShader(bounds);
+          },
+          child:  Text(
+            "Last Hope",
+            style: TextStyle(
+              fontSize: 68,
+              fontWeight: FontWeight.w900,
+              fontStyle: FontStyle.italic,
+              fontFamily: 'Cursive',
+              color: Colors.grey.shade300,
+              letterSpacing: 1,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
